a96cbc57c4f4f228531286b46569f130bf9bf9a4
Vigneshwar S B <vigneshwar.sb@mrcooper.com>
2023-11-05 09:48:05 +0530

    Add react routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route }
+    from 'react-router-dom';
 import Notes from "./Notes";
-import ClassComponent from './ClassComponent';
 import FunctionalComponent from "./FunctionalComponent";
 function App() {
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <ClassComponent />
-        <FunctionalComponent />
-        <Notes />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route exact path='/' element={<Notes />} />
+                    <Route path='/notes' element={<Notes />} />
+                    <Route path='/func' element={<FunctionalComponent />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
