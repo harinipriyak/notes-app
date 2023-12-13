ca580e88200b49d46f96e8f1f0f575fbf6a5e042
Harini Priya K <hk5@MACCL2Y2626LXJC.local>
2023-12-13 10:39:04 +0530

    WIP test-function-app-azure

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
+const path = require('path');
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
@@ -15,24 +15,33 @@ app.use((req, res, next) => {
     next();
 });
 
-app.listen(80, () => console.log(`Running on PORT 8080`));
+// app.listen(80, () => console.log(`Running on PORT 8080`));
+
+app.use(express.static('build'));
 
 app.use(bodyParser.json());
 
-app.get('/api', (req,res,next) => {
-    res.send({status: 200, message: 'you have reached api'});
-});
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    }); 
 
-app.get('/status', (req,res,next) => {
-    res.send({status: 200, message: 'you have reached status'});
-});
+// app.get('/api', (req,res,next) => {
+//     res.send({status: 200, message: 'you have reached api'});
+// });
 
-app.get('/api/v1/', (req,res,next) => {
-    res.send({status: 200, message: 'you have reached api v1'});
-});
+// app.get('/status', (req,res,next) => {
+//     res.send({status: 200, message: 'you have reached status'});
+// });
 
-app.get('/api/v2/', (req,res,next) => {
-    res.send({status: 200, message: 'you have reached api v2'});
-});
+// app.get('/api/v1/', (req,res,next) => {
+//     res.send({status: 200, message: 'you have reached api v1'});
+// });
+
+// app.get('/api/v2/', (req,res,next) => {
+//     res.send({status: 200, message: 'you have reached api v2'});
+// });
+const PORT = process.env.PORT || 5000;
+console.log('server started on port:',PORT);
+app.listen(PORT);
 
 module.exports = {app};
